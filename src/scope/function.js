c060c0a75afe1908c34b5e2f0cd8ac8df65349f0
Paul Montoya <paulmontoya15@gmail.com>
2022-06-12 19:00:28 -0500

    la asignacion de variables dentro de funciones trabaja igual que en el scope global. Importante es dejar de usar VAR en nuestros proyectos y evitar la re asignacion de valores en las variables

---
+++ src/scope/function.js
@@ -0,0 +1,16 @@
+const fuits = () => {
+  var fruit = 'apple'; 
+  console.log(fruit)
+};
+
+fuits();
+
+const anotherFunction = () => {
+  var x = 1;
+  var x = 2;
+  let y = 1;
+  //  let y = 2;
+  console.log(x);
+  console.log(y);
+};
+
